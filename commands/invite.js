7dba83c57ad48e6617dbb406dbd2f6fa6707d2fa
Alejandro Cruz <hcruz0716@gmail.com>
2021-07-07 00:35:30 -0500

    se canvia la invitacion del bot

--- commands/invite.js
+++ commands/invite.js
@@ -9,7 +9,7 @@ module.exports = {
       .setColor("#00C4B3")
       .addFields({
         name: `Da click aqui abajo`,
-        value: `[➡ Invitación ⬅](https://discord.com/oauth2/authorize?client_id=862037564985835581&permissions=70282305&scope=bot)`,
+        value: `[➡ Invitación ⬅](https://discord.com/api/oauth2/authorize?client_id=862102104905744394&permissions=70282305&scope=bot)`,
         inline: true
       })
       .setTimestamp();
